1496ef175c888ce6ab703e82d90161c203b555bf
hiranumaty <hiranumaty@noar.co.jp>
2020-12-21 15:11:10 +0900

    ログイン画面表示成功(クロスオリジンやscriptエラーの対処)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 import store from './store'
 import axios from 'axios'
