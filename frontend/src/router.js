39157c9f6dbc653859ef35e510a834ecdd6fb90e
hiranumaty <hiranumaty@noar.co.jp>
2020-12-21 11:09:52 +0900

    Global 及びvue ページ作成中

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,10 +2,72 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import LoginPage from '@/pages/LoginPage'
 //ここに必要なページのテンプレートを記述する
+import HomePage from '@/pages/HomePage'
+import LoginPage from '@/pages/LoginPage'
+
+
 import store from '@/store'
 
 Vue.use(VueRouter)
 
 const router= new VueRouter({
     //先にStoreを完成させて認証の状態の取得を実装すべきか
+    mode:'histroy',
+    routes:[
+        {
+            path: '/',
+            component:HomePage,
+            meta:{requiresAuth:true}
+        },
+        {
+            path:'/login',
+            component:LoginPage
+        },
+        {
+            path:'*',
+            redirect:'/'
+        }
+    ]
 })
+//Routerによる画面遷移の際に毎回実行される
+
+router.beforeEach((to,from,next) =>{
+    const isLoggedIn = store.getters['auth/isLoggedIn']
+    const token = sessionStorage.getItem('token')
+
+    if(to.matched.some(record => record.meta.requiresAuth))
+    {
+        //ログイン状態の場合
+        if(isLoggedIn){
+            next()
+        }else{
+            //トークンが残っているか
+            if(token!=null){
+                store.dispatch('auth/reload')
+                    .then(()=>{
+                        next()
+                    })
+                    .catch(()=>{
+                        forceToLoginPage(to,from,next)
+                    })
+            }else{
+                forceToLoginPage(to,from,next)
+            }
+        }
+    }else{
+        //ログイン不要なページであればすぐに遷移
+        next()
+    }
+})
+//ログイン画面ンへ強制遷移する
+function forceToLoginPage(to,from,next){
+    next({
+        path:'/login',
+        query:{
+            next:to.fullPath
+        }
+    })
+}
+
+export default router
+
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,115 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { Store } from 'vuex'
 
 import api from '@/services/api'
 
 Vue.use(Vuex)
+
+//認証に使用するモジュール
+const authModule = {
+    strict: process.env.NODE_ENV !== 'production',
+    namedspaced:true,
+    state:{
+        isLoggedIn:false,
+        user_id = '',
+    },
+    getters:{
+        isLoggedIn:state => state.isLoggedIn
+    },
+    mutations:{
+        set(state,payload){
+            state.user_id = payload.user_id
+            state.isLoggedIn = true
+        },
+        clear(state){
+            state.isLoggedIn = false
+            state.user_id = ''
+        }
+    },
+    actions:{
+        login(context,payload){
+            var host = process.env.VUE_APP_API_BASE_URL;
+            //ここのURL定義を確認すべし
+            return api.post(host +'/auth/jwt/create/',{
+                'user_id':payload.user_id,
+                'password':payload.password
+            }).then(response =>{
+                sessionStorage.setItem('token',response.data.auth_token)
+                return context.dispatch('reload')
+                    .then(user => user)
+            })
+        },
+        logout(context){
+           sessionStorage.removeItem('token')
+           sessionStorage.removeItem('user')
+           context.commit('clear')
+        },
+        reload(context){
+            var host = process.env.VUE_APP_API_BASE_URL;
+            return api.get(host + '/auth/users/me/')
+                .then(response =>{
+                    const user = response.data
+                    context.commit('set',{user:user})
+                    sessionStorage.setItem('user',JSON.stringify(user))
+                    return user
+                })
+        }
+    }
+}
+
+//メッセージモジュール
+const messageModule ={
+    strict: process.env.NODE_ENV !== 'production',
+    namedspaced:true,
+    state:{
+        error:'',
+        warnings:[],
+        info:''
+    },
+    getters:{
+        error:state => state.error,
+        warnings:state => state.warnings,
+        info: state => state.info
+    },
+    mutations:{
+        set(state,payload){
+            if(payload.error){
+                state.error = payload.error
+            }
+            if(payload.warnings){
+                state.warnings = payload.warnings
+            }
+            if(payload.info){
+                state.info = payload.info
+            }
+        },
+        clear(state){
+            state.error =''
+            state.warnings =[]
+            state.info = ''
+        }
+    },actions:{
+        setErrorMessage(context,payload){
+            context.commit('clear')
+            context.commit('set',{'error':payload.message})
+        },
+        setWarningMessage(context,payload){
+            context.commit('clear')
+            context.commit('set',{'info':payload.message})
+        },
+        setInfoMessage(context,payload){
+            context.commit('clear')
+            context.commit('set',{'info':payload.message})
+        },
+        clearMessages(context){
+            context.commit('clear')
+        }
+    }
+}
+const store = new Vuex.Store({
+    modules:{
+        auth:authModule,
+        message:messageModule
+    }
+})
+export default store
