b3d2bf3430b49ed636dd37596c96090458346000
hiranumaty <hiranumaty@noar.co.jp>
2021-02-15 12:01:56 +0900

    修整

--- ExState/urls.py
+++ ExState/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-
 from . import views
-
 #ExStateにアクセスするためには認証情報をヘッダーに付与して送信する必要がある
 urlpatterns = [
     #一覧取得
     path('ExState/',views.ExStateListAPIView.as_view()),
-    path('ExState/<str:deploy_id>/<str:Task_id>',views.ExStateRetriveAPIView.as_view())
+    path('ExState/<str:deploy_id>/Detail/<str:Task_id>',views.ExStateRetriveAPIView.as_view())
     #選択したモデルの変更
 ]
