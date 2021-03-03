04dc22839acf43514c9795613d220c58ca051bda
hiranumaty <hiranumaty@noar.co.jp>
2021-03-03 15:24:45 +0900

    よくわからん

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -22,16 +22,15 @@ export default{
         return{
             taskLength:"",
             taskListColumns:[],
-            toDoDatas:[],
             DeptList:[],
+            toDoDatas:[],
         }
     },
     created(){
         this.host = process.env.VUE_APP_API_BASE_URL;
-        this.userinfo = JSON.parse(sessionStorage.getItem("user"));
-        this.getTaskMaster();
-        this.getDepts();
-        this.getStatusData("202103");
+        this.userinfo = JSON.parse(sessionStorage.getItem("user"))
+        this.getTaskMaster()
+        this.getStatusData("202103")
     },
     methods:{
         getTaskMaster(){
@@ -49,26 +48,26 @@ export default{
             });
         },
         getDepts(){
+            let List = []
             api.get(this.host+"/GetDeptsMaster/")
             .then((response)=>{
-                let List = []
                 let dept_length = response.data.length
                 for(let counter=0;counter<dept_length;counter++){
                     let data = response.data[counter]
                     List.push(data["id"])
                 }
-                this.DeptList = List
-                console.log(this.DeptList)
             });
+            return List;
         },
         getStatusData(month){
-            for(let counter=0;this.DeptList.length;counter++){
-                let select_dept = this.DeptList[counter]
-                api.get(this.host+"/ExState/"+month+"/"+select_dept+"/list/")
+            let DeptList = this.getDepts();
+            console.log(DeptList)
+            for(let dept in DeptList){
+                api.get(this.host+"/ExState/"+month+"/"+dept+"/list/")
                 .then((response)=>{
                     console.log(response)
                 });
-            }
+            }  
         },
     }
 };
