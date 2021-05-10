b691460f79ca6200ffa083ca0f800b534c3bca50
hiranumaty <hiranumaty@noar.co.jp>
2021-05-10 13:39:28 +0900

    requestsに変更

--- ExState/management/commands/makeExState.py
+++ ExState/management/commands/makeExState.py
@@ -1,5 +1,6 @@
 import datetime
-import pycurl,json,ast
+import pycurl,json
+import requests
 from django.core.management import BaseCommand
 from io import BytesIO
 from . import settings
@@ -18,48 +19,24 @@ class Command(BaseCommand):
         '''ログインしてaccessトークンを取得する
         この際にbufferに出力してresponseのaccessトークンを取得する
         '''
-        buffer = BytesIO()
-        login_header = ['Content-Type:application/json','Accept:application/json']
+        login_header = {'Content-Type':'application/json','Accept':'application/json'}
         login_data = json.dumps({"user_id":uid,"password":password})
-        curl = pycurl.Curl()
-        curl.setopt(pycurl.URL,url)
-        curl.setopt(pycurl.HTTPHEADER,login_header)
-        curl.setopt(pycurl.POST,True)
-        curl.setopt(pycurl.POSTFIELDS,login_data)
-        curl.setopt(pycurl.WRITEDATA,buffer)
-        curl.setopt(pycurl.CONNECTTIMEOUT,10)
-        curl.perform()
-        response = curl.getinfo(pycurl.HTTP_CODE)
-        curl.close()
-        if response == 200:
-            response = buffer.getvalue()
-            data= json.loads(response)
-            buffer.close()
+        response = requests.post(url,headers=login_header,data=login_data)
+        if response.status_code == 200:
+            text = response.text
+            data= json.loads(text)
             return data['access']
         else:
             return ''
+
     def ExRegist(self,token,yearmonth):
         #ここが止まらない問題is 何
-        buffer = BytesIO()
-        curl = pycurl.Curl()
-        Ex_header = ['Content-Type:application/json','Authorization:JWT '+token,'Accept:application/json']
-        curl.setopt(pycurl.URL,'http://localhost:8000/api/v1/ExState/CreateState/'+yearmonth+'/')
-        curl.setopt(pycurl.HTTPHEADER,Ex_header)
-        curl.setopt(pycurl.WRITEDATA,buffer)
-        curl.setopt(pycurl.POST,True)
-        curl.setopt(pycurl.TIMEOUT_MS,500)
-        curl.perform()
-        curl.close()
-        #ここで何を待っているんだ?
-        #response = curl.getinfo(pycurl.HTTP_CODE)
-        # if response == 200:
-        #     print('作成が完了しました')
-        #     return True
-        # else:
-        #     print('作成失敗です')
-        #     return False
-        #HTTPステートを取得して結果を出したいけれど...なぜかおわらない
-
+        Ex_header = {'Content-Type':'application/json','Authorization':'JWT '+token,'Accept':'application/json'}
+        response = requests.post('http://localhost:8000/api/v1/ExState/CreateState/'+yearmonth+'/',headers=Ex_header)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
 
     def handle(self,*args,**options):
         year = int(options['TargetMonth'][0:4])
@@ -73,8 +50,8 @@ class Command(BaseCommand):
                 login_url = 'http://localhost:8000/api/v1/auth/jwt/create'
                 login_token = self.loginConnect(login_url,uid,password)
                 if  not login_token == "":
+                    print(login_token)
                     self.ExRegist(login_token,options['TargetMonth'])
-                    sys.exit()
                 else:
                     print('ログインできませんでしたIDとパスワードを確認してください')
             else:
