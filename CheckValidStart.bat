e152348d25dd07ccdbfcb92e8ef3123b32384c91
hiranumaty <hiranumaty@noar.co.jp>
2021-05-17 15:48:42 +0900

    パッケージ追加

--- CheckValidStart.bat
+++ CheckValidStart.bat
@@ -1,3 +1,4 @@
 @echo off
 set date_str = %date:~-10,4%%date:~-5,2%%date:~-2,2%
-python manage.py CheckValid_Start %date_str%
+rem python manage.py CheckValid_Start %date_str%
+pipenv run python3 manage.py CheckValid_Start %date_str%
--- CreateTask.bat
+++ CreateTask.bat
@@ -1,3 +1,4 @@
 @echo off
 set date_str = %date:~-10,4%%date:~-5,2%
-python manage.py makeExState %date_str%
+rem python manage.py makeExState %date_str%
+pipenv run python3 manage.py makeExState %date_str%
